8020f2d8137e6a62fc257a073c620b6920c5c0e8
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-05-02 01:27:35 +0530

    Version : 2.1.1 : Added db from our app.js and added middleware and listening port in 9000.

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// http://localhost:9000/persons
 // Url of our app here
-const url = 'mongodb://localhost/PersonsDb';
+const url = 'mongodb://localhost/PersonDb';
+
 // Connecting data base here; To avoid warning using { useNewUrlParser: true }
 mongoose.connect(url, { useNewUrlParser: true });
+
 // Handling database here
 const connection = mongoose.connection;
-connection.on('open', () => { console.log('Connected...') });
+connection.on('open', () => { console.log('Connected with database MongoDB...') });
+
+// Adding middle ware from here
+app.use(express.json());
+
+const personRouter = require('./routes/persons');
+app.use('/persons', personRouter);
 
 // Server listening here
-app.listen(9000);
+app.listen(9000, () => { console.log('Express Server Started...') });
