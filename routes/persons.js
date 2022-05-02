6c233a76d26a1799cc07f00e5c6ac12e0eb0d245
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-05-03 00:41:41 +0530

    Version : 3.1.1 : Added conditions for updating different types of values.

--- routes/persons.js
+++ routes/persons.js
@@ -27,26 +27,35 @@ router.patch('/:id', async (req, res) => {
     try {
         // Finding person by their id.
         const person = await Person.findById(req.params.id);
-        // Changing value here
-        person.have_job = req.body.have_job;
-        // Saving value here
-        const update_have_job = await person.save();
+        let updated_person;
+        if (req.body.name) {
+            // Changing value here
+            person.name = req.body.name;
+            // Saving value here
+            updated_person = await person.save();
+        }
+        if (req.body.programming_language) {
+            // Changing value here
+            person.programming_language = req.body.programming_language;
+            // Saving value here
+            updated_person = await person.save();
+        }
+        if (req.body.age) {
+            // Changing value here
+            person.age = req.body.age;
+            // Saving value here
+            updated_person = await person.save();
+        }
+        if ((req.body.have_job) !== (undefined)) {
+            // Changing value here
+            person.have_job = req.body.have_job;
+            // Saving value here
+            updated_person = await person.save();
+        }
         // Showing value in responce
-        res.json(update_have_job);
+        console.log("Value Changed to ", updated_person);
+        res.json(updated_person);
     } catch (error) { res.send("Patch Error : " + error) };
-});
-
-
-// For deleting particular value from delete.
-router.delete('/:id', async (req, res) => {
-    try {
-        // Finding person by their id.
-        const person = await Person.findById(req.params.id);
-        // Deleting value here
-        const deleted_person = await person.remove(req.params.id);
-        // Showing deleted .value in responce
-        res.json(deleted_person);
-    } catch (error) { res.send("Delete Error : " + error) };
 });
 
 
@@ -61,10 +70,25 @@ router.post('/', async (req, res) => {
     try {
         const data = await person.save();
         // Showing value in responce
+        console.log("Added Value in PersonDB is ", data);
         res.json(data);
     } catch (error) {
         res.send("POST Error : " + error);
     }
+});
+
+
+// For deleting particular value from delete.
+router.delete('/:id', async (req, res) => {
+    try {
+        // Finding person by their id.
+        const person = await Person.findById(req.params.id);
+        // Deleting value here
+        const deleted_person = await person.remove(req.params.id);
+        // Showing deleted .value in responce
+        console.log("Deleted Value from PersonDB is ", deleted_person);
+        res.json(deleted_person);
+    } catch (error) { res.send("Delete Error : " + error) };
 });
 
 // Exported Router to access in app.js
